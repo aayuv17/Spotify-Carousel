7366cd074ba8ce8d0570909302a286f29d74b44c
japneet0411 <60810523+japneet0411@users.noreply.github.com>
2021-04-12 00:20:22 +0530

    carousel Added

---
+++ src/components/Modals/Song.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+import {Modal} from 'react-bootstrap'
+import './Song.scss'
+import './Song.css'
+
+
+function PlayASong(props){
+  return(
+  <Modal
+
+    show={props.show}
+    onHide={props.playMusic}
+    dialogClassName=" modal-90w"
+    aria-labelledby="example-custom-modal-styling-title"
+    centered={true}
+    scrollable={false}
+  >
+    
+    <Modal.Body className="Box">
+    <iframe className="Frame"
+    title="Music" 
+     src="https://open.spotify.com/embed/track/7lPN2DXiMsVn7XUKtOW1CS"
+     width="600" 
+     height="500" 
+     frameborder="0" 
+     allowtransparency="true" 
+     allow="encrypted-media">
+       
+     </iframe>
+    </Modal.Body>
+  </Modal>)
+}
+export default PlayASong;
