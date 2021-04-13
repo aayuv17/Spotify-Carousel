6ce1d8a4d3dbfcc665e7295771ca1e28be62e589
aayuv17 <aayuv17@gmail.com>
2021-04-13 17:08:34 +0530

    fixed carousel

--- src/components/Items/Carousel-items.jsx
+++ src/components/Items/Carousel-items.jsx
@@ -1,21 +1,19 @@
 import React from "react"
-import {Carousel} from 'react-bootstrap'
 import './Carousel-items.scss'
 import './Carousel-items.css'
+
+
 function Items(props){
 return(
-    <div className="Card">
-    <img
-      className="img"
-      src={props.image}
-      alt="SONGS"
-      onClick={props.playMusic}
-    />
-    <Carousel.Caption className="caption">
-      <h3>{props.label}</h3>
-      <p>{props.txt}</p>
-    </Carousel.Caption>
-  </div>
+    <div>
+      <img src={props.src} alt={props.alt} className='standard-size'/>
+      <div className='playlist-title'>
+        {props.title}
+      </div>
+      <div className='description'>
+        {props.description}
+      </div>
+    </div>
   )
 }
 
--- src/sections/carousel/carousel.jsx
+++ src/sections/carousel/carousel.jsx
@@ -1,82 +1,13 @@
-import React, { useState } from "react"
+import React from "react"
 import './carousel.scss'
 import './carousel.css'
-import {Carousel,Container} from 'react-bootstrap'
-import indoor from '../../Images/indoor.jpg'
-import Forest from '../../Images/forest.jpg'
-import Butterfly from '../../Images/butterfly.jpg'
-import Sky from '../../Images/sky.jpg'
-import Galaxy from '../../Images/galaxy.jpg'
-import Items from '../../components/Items/Carousel-items'
-import Song from '../../components/Modals/Song';
+import Carousel from './../../components/Carousel/Carousel';
 
 function Slider() {
-  // const [display,setdisplay]=useState(false)
-  const [show, setShow] = useState(false);
-  function playMusic()
-  {
-     if (show===false)
-        setShow(true);
-      else
-        setShow(false);
-  }
-return(
-<div>
- <Song
-   show= {show}
-   playMusic={playMusic}
-/>
-
-
-<Container fluid className="back">
-<Carousel
-style={{opacity:show? 0.5:1}}>
-  <Carousel.Item>
-  <Items 
-    image={indoor}
-    label="First slide label"
-    txt="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    playMusic={playMusic}
-  />
-  </Carousel.Item>
-  <Carousel.Item>
-  <Items 
-    image={Butterfly}
-    label="Second slide label"
-    txt="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    playMusic={playMusic}
-  />
-  </Carousel.Item>
-  <Carousel.Item>
-  <Items 
-    image={Sky}
-    label="Third slide label"
-    txt="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    playMusic={playMusic}
-  />
-  </Carousel.Item>
-  <Carousel.Item>
-  <Items 
-    image={Galaxy}
-    label="Fourth slide label"
-    txt="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    playMusic={playMusic}
-  />
-  </Carousel.Item>
-  <Carousel.Item>
-  <Items 
-    image={Forest}
-    label="Fourth slide label"
-    txt="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    playMusic={playMusic}
-  />
-  </Carousel.Item>
-  
- 
-</Carousel>
-
-</Container>
-</div>
-)
+  return(
+    <div>
+        <Carousel />
+    </div>
+  )
 }
 export default Slider;
